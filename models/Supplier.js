441c10de706668417872a30dd686f61061faf323
myuyen0304 <134291922+myuyen0304@users.noreply.github.com>
2025-09-20 10:47:14 +0700

    Delete models directory

--- models/Supplier.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-const supplierSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  address: { type: String, required: true },
-  phone: { type: String, required: true },
-});
-module.exports = mongoose.model("Supplier", supplierSchema);
